9fe5a9042216d898739535967b9424416e6f75b2
witeoutmysin <lorenzocovarrubiasjr@gmail.com>
2021-03-03 23:39:00 -0600

    Removed unneccessary files, created Login page, and spotify auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+import { getTokenFromUri } from "./spotify";
+
+import Login from "./Containers/Login/Login";
 
 function App() {
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUri();
+    console.log("Hash: ", hash);
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+    }
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Login />
     </div>
   );
 }
